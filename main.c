b808a22cc67cce949aa0710e0ca7e51301c3b492
StamosArhon <stamosarhontis@gmail.com>
2019-03-13 23:48:50 +0200

    Added the function call to 'read_faces' as well as a 'printGenericTree' function call for the 'FacesTree', also modified some aspects of the 'read_vertex' function call, specifically added an argument called 'last_vertex_position' that I can later pass to the 'read_faces' function in order to start the reading of the file from the first line of the faces list

--- main.c
+++ main.c
@@ -5,25 +5,37 @@
 /*-----------Included Project Files-----------*/
 #include "header.h"
 #include "vertex.h"
+#include "faces.h"
 #include "ply_file_data_tree.h"
 /*-----------Included Project Files-----------*/
 
 int main(void)
 {
   int line_change = 0;
+  int *last_vertex_position;
   Header *p_header = malloc(sizeof(Header));
   GenericTree *VertexTree = NewGenericTree();
+  GenericTree *FacesTree = NewGenericTree();
 
-  read_header("cube.ply", p_header);
+  read_header ("cube.ply", p_header);
+  read_vertices ("cube.ply", VertexTree, p_header->elements[EL_VERTEX], p_header->el_props[EL_VERTEX], last_vertex_position);
+  read_faces ("cube.ply", FacesTree, p_header->elements[EL_FACE], last_vertex_position);
 
-  read_vertices("cube.ply", VertexTree, p_header->elements[EL_VERTEX], p_header->el_props[EL_VERTEX]);
-
-  /*------Print Tree------*/
+  /*------Print Vertex Tree------*/
   printf ("\n=========");
   printf ("\nVertices:");
   printf ("\n=========\n\n");
   printGenericTree (VertexTree->root, line_change, p_header->el_props[EL_VERTEX]);
-  /*------Print Tree------*/
+  /*------Print Vertex Tree------*/
+
+  printf ("\n\n");
+
+  /*------Print Faces Tree------*/
+  printf ("\n======");
+  printf ("\nFaces:");
+  printf ("\n======\n\n");
+  printGenericTree (FacesTree->root, line_change, 5);
+  /*------Print Faces Tree------*/
 
 
   /*------Memory clean-up------*/
