ae10441c08537347b762e8ac4833246f72a208a2
StamosArhon <stamosarhontis@gmail.com>
2019-03-19 21:11:28 +0200

    Made modifications to the printfaces function definition

--- faces.h
+++ faces.h
@@ -4,6 +4,6 @@
 #include "ply_file_data_tree.h"
 
 void read_faces (char *filename, GenericTree *FacesTree, int faces_elements, int *last_vertex_position);
-void print_faces (GenericTree *VertexTree, GenericTree *FacesTree);
+void print_faces (GenericTree *VertexTree, GenericTree *FacesTree, int faces_elements);
 
 #endif
