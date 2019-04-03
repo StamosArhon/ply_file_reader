6a958e04762f43adda0fc31f602bc294ee51e75c
StamosArhon <stamosarhontis@gmail.com>
2019-04-03 03:56:44 +0300

    Added the parameter "faces elements" that was used as iterator in the "for" loops inside the print_faces function

--- faces.h
+++ faces.h
@@ -4,6 +4,6 @@
 #include "ply_file_data_tree.h"
 
 void read_faces (char *filename, GenericTree *FacesTree, int faces_elements, int *last_vertex_position);
-void print_faces (GenericTree *VertexTree, GenericTree *FacesTree, int faces_elements);
+void print_faces (GenericTree *VertexTree, GenericTree *FacesTree, int faces_elements, int vertex_properties);
 
 #endif
