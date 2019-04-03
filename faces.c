a516e2d0268ac29a65b76d51794338dd7d3be451
StamosArhon <stamosarhontis@gmail.com>
2019-04-03 04:00:25 +0300

    Added more options to the faces_sub_id_array in case we have a file with more face indices. Also fixes a bug in the was the expanded faces tree was printed and a bug caused by one of my variables pointing to a non existent memory location. The program now runs as expected

--- faces.c
+++ faces.c
@@ -18,7 +18,12 @@ void read_faces (char *filename, GenericTree *FacesTree, int faces_elements, int
   int id = 1;
   float *faces_data = malloc((faces_elements*MAX_LINE_SIZE)*sizeof(float));
   float faces_index_number;
-  char *faces_sub_id [5] = {"a", "b", "c", "d", "e"};
+  char *faces_sub_id [24] = {
+                             "a", "b", "c", "d", "e", "f", "g"
+                             "h", "i", "j", "k", "l", "m", "n"
+                             "o", "p", "q", "r", "s", "t", "u"
+                             "v", "w", "x", "y", "z"
+                            };
 
   while ((read = getline(&line, &line_sz, input)) >= 0)
   {
@@ -47,15 +52,20 @@ void read_faces (char *filename, GenericTree *FacesTree, int faces_elements, int
   }
 }
 
-void print_faces (GenericTree *VertexTree, GenericTree *FacesTree, int faces_elements)
+/*NOTE: Mistake-bug made by me when designing the function.
+  It does not read the faces data correctly. Perfect the desing*/
+
+void print_faces (GenericTree *VertexTree, GenericTree *FacesTree, int faces_elements, int vertex_properties)
 {
   GenericTreeNode *Iterator = malloc(sizeof(GenericTreeNode));
+  GenericTreeNode *Iterator2 = malloc(sizeof(GenericTreeNode));
   GenericTreeNode *TempNode = malloc(sizeof(GenericTreeNode));
-  char *vertex_sub_id[3] = {"x", "y", "z"};
-  char *faces_sub_id[4] = {"a", "b", "c", "d"};
-  int count;
-  int i,j;
 
+  char *vertex_sub_id[3] = {"x", "y", "z"};
+  char *faces_sub_id[5] = {"a", "b", "c", "d", "e"};
+  int count, faces_number;
+  int i,j,k;
+  float vertex_d;
 
   printf ("\n====================");
   printf ("\nExpanded Faces List:");
@@ -65,15 +75,32 @@ void print_faces (GenericTree *VertexTree, GenericTree *FacesTree, int faces_ele
   {
     Iterator = FindGenericTreeNode(i+1, faces_sub_id[0], FacesTree);
     count = (int)(*((float*)Iterator->data));
+
     printf ("|Face number %d|:\t", i+1);
 
-    for (j=0; j<count-1; j++)
+    for (j=1; j <= count; j++)
     {
-      TempNode = FindGenericTreeNode(i+1, vertex_sub_id[j], VertexTree);
-      printf("\n|%d%s\t%.1f|", i+1, vertex_sub_id[j], *((float *)TempNode->data));
+      Iterator2 = FindGenericTreeNode(i+1, faces_sub_id[j], FacesTree);
+      faces_number = (int)(*((float*)Iterator2->data));
+
+      printf ("\n\n+=========================================+");
+      printf ("\nFaces Number [%d] - Index [%d] Composition:", i+1, faces_number);
+      printf ("\n+=========================================+");
+
+      printf ("\n\n+---------------------------------------+\n");
+
+      for (k=0; k < vertex_properties; k++)
+      {
+        TempNode = FindGenericTreeNode(faces_number+1, vertex_sub_id[k], VertexTree);
+        vertex_d = (*((float*)TempNode->data));
+        printf("\n|%d%s %.1f|", faces_number+1, vertex_sub_id[k], vertex_d);
+      }
+
+      printf ("\n\n+---------------------------------------+\n");
     }
     printf("\n\n");
   }
   free(Iterator);
+  free(Iterator2);
   free(TempNode);
 }
